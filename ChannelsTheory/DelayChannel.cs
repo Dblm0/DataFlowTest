163711726bf5cfa68b979e3338819c44c3b14f07
DmitrievDV <13@13.tt>
2020-11-20 15:27:40 +0300

    add ChannelsTheory project

---
+++ ChannelsTheory/DelayChannel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelsTheory
+{
+    public class DelayChannel<T> : Channel<T>
+    {
+        public DelayChannel(int millisecondsDelay)
+        {
+            PacketsDelayMilliseconds = millisecondsDelay;
+            Reader = _output;
+            Writer = _input;
+
+            Task.Run(async () =>
+            {
+                ChannelReader<T> inpReader = _input;
+                ChannelWriter<T> outWriter = _output;
+                await foreach (var item in inpReader.ReadAllAsync().ConfigureAwait(false))
+                {
+                    await Task.WhenAll(outWriter.WriteAsync(item).AsTask(), Task.Delay(PacketsDelayMilliseconds));
+                }
+                outWriter.Complete();
+            });
+        }
+        public int PacketsDelayMilliseconds { get; }
+        Channel<T> _input = Channel.CreateUnbounded<T>();
+        Channel<T> _output = Channel.CreateBounded<T>(1);
+    }
+}

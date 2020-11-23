22e3ba7489915793693a952b169f6db28fbb06d0
DmitrievDV <13@13.tt>
2020-11-23 16:40:56 +0300

    move MeasurmentsData to single file

---
+++ DataFlowTest/MeasurmentsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFlowTest
+{
+    class MeasurmentsData
+    {
+        public MeasurmentsData(IReadOnlyDictionary<int, List<double>> delays, List<double> sequence)
+        {
+            Delays = delays;
+            Sequence = sequence;
+        }
+
+        public IReadOnlyDictionary<int, List<double>> Delays { get; }
+        public List<double> Sequence { get; }
+    }
+}
--- DataFlowTest/TestHelper.cs
+++ DataFlowTest/TestHelper.cs
@@ -40,17 +40,5 @@ namespace DataFlowTest
                 }
             }
         }
-    }
-
-    class MeasurmentsData
-    {
-        public MeasurmentsData(IReadOnlyDictionary<int, List<double>> delays, List<double> sequence)
-        {
-            Delays = delays;
-            Sequence = sequence;
-        }
-
-        public IReadOnlyDictionary<int, List<double>> Delays { get; }
-        public List<double> Sequence { get; }
-    }
+    }   
 }

f30b6f5c19431de0bde1181583281384ff839c7d
DmitrievDV <13@13.tt>
2020-11-30 16:34:43 +0300

    change test assert criteria

--- DataFlowTest/TestHelper.cs
+++ DataFlowTest/TestHelper.cs
@@ -19,26 +19,28 @@ namespace DataFlowTest
         {
             _helper = helper;
         }
-        public void AssertStats(List<double> values, double baseLine, double drift, int skip = 0)
+        public void AssertStats(List<double> values, double baseLine, double upperLim, int skip = 0)
         {
             var skipped = values.Skip(skip);
             var max = skipped.Max();
             var min = skipped.Min();
+            var maxRestriction = (Value: max, UpperLim: upperLim, LowerLim: baseLine);
+            var minRestriction = (Value: min, UpperLim: baseLine, LowerLim: baseLine);  //strict minimum control
             _helper.WriteLine($"Target = {baseLine}, (min:{min}; max:{max})");
 
-            foreach (var extreme in new[] { min, max })
+            foreach (var restrict in new[] { minRestriction, maxRestriction })
             {
                 try
                 {
-                    Assert.InRange(extreme, baseLine - drift, baseLine + drift);
+                    Assert.InRange(restrict.Value, restrict.LowerLim, restrict.UpperLim);
                 }
                 catch
                 {
-                    int extremeIdx = values.IndexOf(extreme);
+                    int extremeIdx = values.IndexOf(restrict.Value);
                     _helper.WriteLine($"area around extreme value: {string.Join("; ", values.Skip(extremeIdx - 4).Take(8))}");
                     throw;
                 }
             }
         }
-    }   
+    }
 }

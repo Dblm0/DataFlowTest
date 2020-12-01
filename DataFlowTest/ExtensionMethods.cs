949b59fc951c5f7555bb1ed33436b51922a476c9
DmitrievDV <13@13.tt>
2020-12-01 17:20:20 +0300

    channels removed

--- DataFlowTest/ExtensionMethods.cs
+++ DataFlowTest/ExtensionMethods.cs
@@ -60,47 +60,4 @@ namespace DataFlowTest
             return new MeasurmentsData(stats, seq);
         }
     }
-    static class ChannelExtensions
-    {
-        public static async Task ProduceDataAsync(this ChannelWriter<int> channel, int item, int count, bool completeRequired = false)
-        {
-            foreach (var i in Enumerable.Repeat(item, count))
-            {
-                await channel.WriteAsync(i);
-            }
-            if (completeRequired)
-            {
-                channel.Complete();
-            }
-        }
-        public static async Task<MeasurmentsData> MeasureDataAsync(this ChannelReader<int> channel)
-        {
-            var stats = new List<double>();
-            var seq = new List<double>();
-            Stopwatch sw = Stopwatch.StartNew();
-            await foreach (int x in channel.ReadAllAsync().ConfigureAwait(false))
-            {
-                seq.Add(x);
-                sw.Stop();
-                stats.Add(sw.ElapsedMilliseconds);
-                sw.Restart();
-            }
-            return new MeasurmentsData(new Dictionary<int, List<double>> { [-1] = stats }, seq);
-        }
-        public static async Task<MeasurmentsData> MeasureDataAsync(this ChannelReader<int> channel, int[] keys)
-        {
-            var stopwatches = keys.ToDictionary(x => x, x => new Stopwatch());
-            var seq = new List<double>();
-            var stats = keys.ToDictionary(x => x, x => new List<double>());
-
-            await foreach (int x in channel.ReadAllAsync().ConfigureAwait(false))
-            {
-                seq.Add(x);
-                stopwatches[x].Stop();
-                stats[x].Add(stopwatches[x].ElapsedMilliseconds);
-                stopwatches[x].Restart();
-            }
-            return new MeasurmentsData(stats, seq); ;
-        }
-    }
 }
